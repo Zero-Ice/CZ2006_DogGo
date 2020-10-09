d8b541e0b61f929bc43bf5b0e593197565e745a3
shah <h.jshah39@gmail.com>
2020-10-09 14:04:16 +0800

    Added add button UI with dummy dog data

--- DogGoApp/doggo/lib/Routes/DogProfile.dart
+++ DogGoApp/doggo/lib/Routes/DogProfile.dart
@@ -6,19 +6,63 @@ import 'package:flutter/widgets.dart';
 class DogProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    Widget addbutton=  FloatingActionButton(
+      onPressed: () {},
+      child:
+      Text('+',
+        style: TextStyle(
+            fontSize: 28.0,
+            shadows: <Shadow>[Shadow(
+                offset: Offset(1.0, 2.0),
+                blurRadius: 3.0,
+                color: Colors.black45)]),
+      ),
+    );
+
+
+    Widget dog1 = Container(
+        child: Text("hi")
+    );
+
+    Widget dog2 = Container(
+        child: Text("hi")
+    );
+
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("DogProfile"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first screen when tapped.
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+        child: Column(
+            children: [
+              const SizedBox(height: 20),
+              dog1,
+              const SizedBox(height: 20),
+              dog2,
+            ]
         ),
       ),
+
+
+
+      floatingActionButton: Container(
+        height: 65.0,
+        width: 65.0,
+        child: FittedBox(
+            child:addbutton),
+      ),
+
+      /*child:Align(
+          alignment: Alignment.bottomRight,
+          child:Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: floatingActionButton
+          ),)*/
+
+
     );
   }
 }
