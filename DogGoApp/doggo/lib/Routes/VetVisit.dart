2b0b274eadf74503a324ba04dc2c0952880a1286
Bry <xiaocraze@gmail.com>
2020-10-11 13:40:52 +0800

    empty placeholder buttons for vet visits, add, edit, delete

--- DogGoApp/doggo/lib/Routes/VetVisit.dart
+++ DogGoApp/doggo/lib/Routes/VetVisit.dart
@@ -8,17 +8,45 @@ class VetVisit extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("VetVisit"),
+        title: Text("Vet Visit"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first screen when tapped.
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
+      body: Container(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              addVisit,
+              const SizedBox(height: 20),
+              editVisit,
+              const SizedBox(height: 20),
+              delVisit,
+              const SizedBox(height: 20),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: RaisedButton(
+                  onPressed: () {
+          // Navigate back to first screen when tapped.
+          Navigator.pop(context);
+    },
+      child: Text('Go back!'),
+    ),
+              )
+            ],
+          )),
     );
   }
+  Widget addVisit = Container(
+      child: RaisedButton(
+        onPressed: () {},
+        child: Text('Add a timing for vet appointment', style: TextStyle(fontSize: 20)),
+      ));
+  Widget editVisit = Container(
+      child: RaisedButton(
+        onPressed: () {},
+        child: Text('Edit an existing vet appointment', style: TextStyle(fontSize: 20)),
+      ));
+  Widget delVisit = Container(
+      child: RaisedButton(
+        onPressed: () {},
+        child: Text('Delete an existing vet appointment', style: TextStyle(fontSize: 20)),
+      ));
 }
