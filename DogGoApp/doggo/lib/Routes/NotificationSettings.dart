702a01eebf46238c35dbe0ef638ae4f1865252d7
shah <h.jshah39@gmail.com>
2020-10-24 01:00:43 +0800

    Notification on button click

--- DogGoApp/doggo/lib/Routes/NotificationSettings.dart
+++ DogGoApp/doggo/lib/Routes/NotificationSettings.dart
@@ -1,9 +1,37 @@
-
+import 'dart:ffi';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class NotificationSettings extends StatelessWidget {
+class NotificationSettings extends StatefulWidget {
+  @override
+  _NotificationSettingsState createState() => _NotificationSettingsState();
+}
+
+class _NotificationSettingsState extends State<NotificationSettings> {
+  FlutterLocalNotificationsPlugin notif;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    var andrNoti = new AndroidInitializationSettings('doggo_notif_icon');
+    var initSetting = new InitializationSettings(android: andrNoti);
+    notif = new FlutterLocalNotificationsPlugin();
+    notif.initialize(initSetting, onSelectNotification: doThis);
+
+  }
+
+  Future _showNotif() async{
+    var androidDets = new AndroidNotificationDetails("channelId", "doggo", "channelDescription",importance: Importance.max);
+    var genDet = new NotificationDetails(android: androidDets);
+    print("notif bef");
+    await notif.show(0, "title", "body", genDet);
+    print("notif ret");
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,14 +39,26 @@ class NotificationSettings extends StatelessWidget {
         title: Text("Notification Settings"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first screen when tapped.
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+        child: Row(
+          children: [
+            RaisedButton(
+              onPressed: () {
+                // Navigate back to first screen when tapped.
+                Navigator.pop(context);
+              },
+              child: Text('Go back!'),
+            ),
+            RaisedButton(
+              onPressed: _showNotif,
+              child: Text('Click Me!'),
+            )
+          ],
         ),
       ),
     );
+  }
+
+  Future doThis(String stuff) async{
+
   }
 }
